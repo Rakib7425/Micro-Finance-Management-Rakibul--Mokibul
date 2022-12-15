4628fe50b1eb746a0f2f0a8519f578a02c4b488c
Rakibul Islam <103498045+Rakib7425@users.noreply.github.com>
2022-12-15 15:23:13 +0530

    deleted:    assets/DataTables/DataTables-1.10.21/css/dataTables.bootstrap.css 	deleted:    assets/DataTables/DataTables-1.10.21/css/dataTables.bootstrap.min.css 	deleted:    assets/DataTables/DataTables-1.10.21/css/dataTables.bootstrap4.css 	deleted:    assets/DataTables/DataTables-1.10.21/css/dataTables.bootstrap4.min.css 	deleted:    assets/DataTables/DataTables-1.10.21/css/dataTables.foundation.css 	deleted:    assets/DataTables/DataTables-1.10.21/css/dataTables.foundation.min.css 	deleted:    assets/DataTables/DataTables-1.10.21/css/dataTables.jqueryui.css 	deleted:    assets/DataTables/DataTables-1.10.21/css/dataTables.jqueryui.min.css 	deleted:    assets/DataTables/DataTables-1.10.21/css/dataTables.semanticui.css 	deleted:    assets/DataTables/DataTables-1.10.21/css/dataTables.semanticui.min.css 	deleted:    assets/DataTables/DataTables-1.10.21/css/desktop.ini 	deleted:    assets/DataTables/DataTables-1.10.21/css/jquery.dataTables.css 	deleted:    assets/DataTables/DataTables-1.10.21/css/jquery.dataTables.min.css 	deleted:    assets/DataTables/DataTables-1.10.21/images/sort_asc.png 	deleted:    assets/DataTables/DataTables-1.10.21/images/sort_asc_disabled.png 	deleted:    assets/DataTables/DataTables-1.10.21/images/sort_both.png 	deleted:    assets/DataTables/DataTables-1.10.21/images/sort_desc.png 	deleted:    assets/DataTables/DataTables-1.10.21/images/sort_desc_disabled.png 	deleted:    assets/DataTables/DataTables-1.10.21/js/dataTables.bootstrap.js 	deleted:    assets/DataTables/DataTables-1.10.21/js/dataTables.bootstrap.min.js 	deleted:    assets/DataTables/DataTables-1.10.21/js/dataTables.bootstrap4.js 	deleted:    assets/DataTables/DataTables-1.10.21/js/dataTables.bootstrap4.min.js 	deleted:    assets/DataTables/DataTables-1.10.21/js/dataTables.foundation.js 	deleted:    assets/DataTables/DataTables-1.10.21/js/dataTables.foundation.min.js 	deleted:    assets/DataTables/DataTables-1.10.21/js/dataTables.jqueryui.js 	deleted:    assets/DataTables/DataTables-1.10.21/js/dataTables.jqueryui.min.js 	deleted:    assets/DataTables/DataTables-1.10.21/js/dataTables.semanticui.js 	deleted:    assets/DataTables/DataTables-1.10.21/js/dataTables.semanticui.min.js 	deleted:    assets/DataTables/DataTables-1.10.21/js/desktop.ini 	deleted:    assets/DataTables/DataTables-1.10.21/js/jquery.dataTables.js 	deleted:    assets/DataTables/DataTables-1.10.21/js/jquery.dataTables.min.js 	deleted:    assets/DataTables/datatables.css 	deleted:    assets/DataTables/datatables.js 	deleted:    assets/DataTables/datatables.min.css 	deleted:    assets/DataTables/datatables.min.js 	deleted:    assets/DataTables/desktop.ini 	deleted:    assets/vendor/jquery/jquery.min.js 	new file:   assets/vendor/jquery/jquery_new.js 	new file:   assets/vendor/jquery/jquery_old.js 	modified:   header.php

--- header.php
+++ header.php
@@ -4,23 +4,22 @@
 <!-- Google Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-<!-- Template Main CSS File -->
+<!--  Template ? Main CSS File -->
 <link href="assets/css/style.css" rel="stylesheet">
 <link type="text/css" rel="stylesheet" href="assets/css/jquery-te-1.4.0.css">
 <link href="assets/css/jquery.datetimepicker.min.css" rel="stylesheet">
 <link href="assets/css/select2.min.css" rel="stylesheet">
-<script src="assets/vendor/jquery/jquery.min.js"></script>
-<script src="assets/DataTables/datatables.min.js"></script>
-<link href="assets/DataTables/datatables.min.css" rel="stylesheet">
+<!-- <script src="assets/vendor/jquery/jquery_old.js"></script> -->
+<script src="assets/vendor/jquery/jquery_new.js"></script>
+
 <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
 <script src="assets/vendor/php-email-form/validate.js"></script>
-<script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
 <script type="text/javascript" src="assets/js/select2.min.js"></script> 
 <script type="text/javascript" src="assets/js/jquery.datetimepicker.full.min.js"></script>
 <script type="text/javascript" src="assets/js/jquery-te-1.4.0.min.js" charset="utf-8"></script>
 
-<!-- internet Req -->
 
+<!-- internet Req -->
 <!-- Font Awesome -->
 <script src="https://kit.fontawesome.com/75bc1d4a1a.js" crossorigin="anonymous"></script>
 
@@ -28,10 +27,18 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
+<!-- DataTables -->
+
+<link rel="stylesheet" href="//cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
+<script type="text/javascript" src="//cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js" charset="utf-8"></script>
 
 
 
 <!-- Vendor CSS Files 2 -->
+
+<!-- <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script> -->
+<!-- <script src="assets/DataTables/datatables.min.js"></script> -->
+<!-- <link href="assets/DataTables/datatables.min.css" rel="stylesheet"> -->
 <!-- <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
 <!-- <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet"> -->
 <!-- <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet"> -->
@@ -49,16 +56,3 @@
 
 <!-- jquery CDN-->
 
-<!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
-
-<!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script> -->
-
-
-<!-- <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script> -->
-
-<!-- <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script> -->
-
-<!-- <script src="https://code.jquery.com/color/jquery.color-2.2.0.js" integrity="sha256-gvMJWDHjgDrVSiN6eBI9h7dRfQmsTTsGU/eTT8vpzNg=" crossorigin="anonymous"></script> -->
-
-<!-- <script src="https://code.jquery.com/pep/0.4.3/pep.js" integrity="sha256-yZpZULjaPllFSRFfS6JsDvucyRd3yNo7yKc/YsMQAsk=" crossorigin="anonymous"></script> -->
